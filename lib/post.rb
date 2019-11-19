2bf79ab01451c0f09304e9ec4088926046a4806e
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-19 21:48:28 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,15 @@
 class Post 
+  attr_accessor :title
+  
+  @@all = []
   
   def initialize(title)
     @title = title 
-    @@all << title
   end
   
+  def self.all
+    @@all
+  end
   
 end
   
