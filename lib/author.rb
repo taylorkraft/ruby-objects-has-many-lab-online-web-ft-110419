a21e46af64ef73e5df7c6c50d86a2ca94714a3af
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-19 21:57:57 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,6 +6,18 @@ class Author
     @name = name
   end
   
+  def posts 
+    
+  end 
   
+  def add_post(a_post)
+    a_post.author = self 
+  end 
+  
+  def add_post_by_title
+  end
+
+  def post_count
+  end
   
 end
